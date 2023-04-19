e88d8966353174e3139463a9508de40dcc655cb1
Hitesh Jangid <hiteshjangid19@gmail.com>
2023-04-19 13:43:47 +0530

    update run.sh file

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+docker stop orbs-status
+docker rm orbs-status
 
-docker run -d --name orbs-status -p 8000:80 -v $(pwd):/var/www/html orb-status
+docker run -d --name orbs-status -p 8001:80 \
+-v $(pwd):/var/www/html \
+-v $(pwd)/php/php.ini-development:/usr/local/etc/php/php.ini-development \
+-v $(pwd)/php/php.ini-production:/usr/local/etc/php/php.ini-production \
+orb-status
 
