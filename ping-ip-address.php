a5dca58fdb21646b95b10f4e67be61da0a5716c5
Hitesh Jangid <hiteshjangid19@gmail.com>
2023-04-25 20:40:02 +0530

    echo the logs

--- ping-ip-address.php
+++ ping-ip-address.php
@@ -7,13 +7,13 @@ function pingIpAddress($ip_address)
   $pingCommand = "ping $ip_address -c 2";
   $pingresult = exec($pingCommand, $outcome, $status);
   
-  // echo "pingCommand - $pingCommand<br>";
-  // echo "status - $status<br>";
-  // echo "pingresult - $pingresult<br>";
-  // echo "outcome - $outcome<br>";
-  // print_r($outcome);
-  // echo "The IP address, $ip_address, is  $status<br>";
-  // echo "------------------";
+  echo "pingCommand - $pingCommand<br>";
+  echo "status - $status<br>";
+  echo "pingresult - $pingresult<br>";
+  echo "outcome - $outcome<br>";
+  print_r($outcome);
+  echo "The IP address, $ip_address, is  $status<br>";
+  echo "------------------";
 
   /* IN PING WE RECEIVE 0 WHEN IT IS SUCESS AND 0 WHEN IT FAILED */
   $timestampQuery = '';
