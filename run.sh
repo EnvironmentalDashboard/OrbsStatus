f11fcafb5ae34c7a51ad79cdb0b7b105938247c0
Hitesh Jangid <hiteshjangid19@gmail.com>
2023-12-12 21:27:57 +0530

    mount secret directory

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ docker rm ORBS_STATUS
 
 docker run -dit -p 80:80 --restart unless-stopped \
   -v $(pwd)/:/var/www/html/ \
+  -v /var/secret/:/var/secret/ \
   --name ORBS_STATUS orbs-status
 
 docker exec ORBS_STATUS cron
