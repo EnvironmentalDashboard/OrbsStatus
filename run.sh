d8151b6db64f6e6ce3e07fef79a14e92c0a596c4
Hitesh Jangid <hiteshjangid19@gmail.com>
2023-12-12 22:18:46 +0530

    mount .git directory into container

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ docker rm ORBS_STATUS
 
 docker run -dit -p 80:80 --restart unless-stopped \
   -v $(pwd)/:/var/www/html/ \
+  -v $(pwd)/.git:/var/www/html/.git \
   -v /var/secret/:/var/secret/ \
   --name ORBS_STATUS orbs-status
 
