a6a1fa4e488003f9f4e25de96b6d17a8463afc5b
Hitesh Jangid <hiteshjangid19@gmail.com>
2023-12-18 22:09:24 +0530

    test null value with data type

--- run-orbs.php
+++ run-orbs.php
@@ -6,7 +6,7 @@ require 'db.php';
 $result = $db->query('SELECT o.id as orb_id, name,inet_ntoa(ip) as ip_address, disabled, water_uuid, elec_uuid, elec_rvid, water_rvid, o.last_connectioned_on, r1.relative_value as elec_rv, r2.relative_value as water_rv, testing FROM orbs o LEFT JOIN relative_values r1 ON r1.id = o.elec_rvid LEFT JOIN relative_values r2 ON r2.id = o.water_rvid WHERE o.disabled = 0 ORDER BY `name`');
 foreach ($result as $row) {
   if ($row['disabled'] == '0') {
-    if ($row['elec_rv'] == null) {
+    if ($row['elec_rv'] === null) {
       $elec = 'X';
     } else {
       // $stmt = $db->prepare('SELECT relative_value FROM relative_values WHERE id = ?');
@@ -14,7 +14,7 @@ foreach ($result as $row) {
       // $elec = round(($stmt->fetchColumn() / 100) * 4); // must be integer 0-4
       $elec = round(($row['elec_rv'] / 100) * 4); // must be integer 0-4
     }
-    if ($row['water_rv'] == null) {
+    if ($row['water_rv'] === null) {
       $water = 'X';
     } else {
       $water = round(($row['water_rv'] / 100) * 4); // must be integer 0-4
